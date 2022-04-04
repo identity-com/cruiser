27f7e8dc2be0b26b2206a20872174e77ddb82f28
Brett Etter <bbetter9101@gmail.com>
2022-04-04 11:44:42 -0500

    Revert "Added `OnChainSize` for non-zero numbers"

--- src/traits/instruction_list.rs
+++ src/traits/instruction_list.rs
@@ -5,7 +5,6 @@ pub use cruiser_derive::InstructionList;
 use crate::account_argument::AccountInfoIterator;
 use crate::account_list::AccountList;
 use crate::compressed_numbers::CompressedNumber;
-use crate::instruction::Instruction;
 use crate::{CruiserResult, SolanaInstruction};
 use solana_program::pubkey::Pubkey;
 
@@ -16,22 +15,14 @@ pub trait InstructionList: Copy {
     /// The accounts for this list of instructions
     type AccountList: AccountList;
 
+    /// Gets the discriminant for the instruction
+    fn discriminant(self) -> u64;
+    /// Gets the discriminant in compressed form
+    fn discriminant_compressed(self) -> Self::DiscriminantCompressed {
+        Self::DiscriminantCompressed::from_number(self.discriminant())
+    }
     /// Creates the instruction from a discriminant
     fn from_discriminant(discriminant: u64) -> Option<Self>;
-}
-/// Allows an instruction list to support an instruction type
-///
-/// # Safety
-/// Implementor must guarantee that no two discriminates match
-pub unsafe trait InstructionListItem<I>: Sized + InstructionList {
-    /// Gets the discriminant for the instruction
-    #[must_use]
-    fn discriminant() -> u64;
-    /// Gets the discriminant in compressed form
-    #[must_use]
-    fn discriminant_compressed() -> Self::DiscriminantCompressed {
-        Self::DiscriminantCompressed::from_number(Self::discriminant())
-    }
 }
 
 /// A Processor for a given [`InstructionList`].
@@ -46,11 +37,7 @@ pub trait InstructionListProcessor<AI, IL: InstructionList> {
 
 /// The basic client function, should have a version of this for each thing you want to be able to cpi.
 /// Also should implement either [`InstructionListCPIStatic`] or [`InstructionListCPIDynamic`].
-pub trait InstructionListCPI {
-    /// The instruction list for this
-    type InstructionList: InstructionListItem<Self::Instruction>;
-    /// The instruction for this
-    type Instruction: Instruction<Self::AccountInfo>;
+pub trait InstructionListCPI<IL: InstructionList> {
     /// The account info this deals with
     type AccountInfo;
 
@@ -60,7 +47,9 @@ pub trait InstructionListCPI {
 }
 
 /// Extension to [`InstructionListCPI`]. More efficient than [`InstructionListCPIDynamic`] but requires statically known account length.
-pub trait InstructionListCPIStatic<const N: usize>: InstructionListCPI {
+pub trait InstructionListCPIStatic<IL: InstructionList, const N: usize>:
+    InstructionListCPI<IL>
+{
     /// Gets the accounts for this call.
     #[must_use]
     fn to_accounts_static<'a>(
@@ -71,13 +60,19 @@ pub trait InstructionListCPIStatic<const N: usize>: InstructionListCPI {
 
 /// Extension to [`InstructionListCPI`].
 /// Less efficient than [`InstructionListCPIStatic`] but can have dynamically sized account length.
-pub trait InstructionListCPIDynamic: for<'a> InstructionListCPIDynamicAccess<'a> {}
-impl<T> InstructionListCPIDynamic for T where T: for<'a> InstructionListCPIDynamicAccess<'a> {}
+pub trait InstructionListCPIDynamic<IL: InstructionList>:
+    for<'a> InstructionListCPIDynamicAccess<'a, IL>
+{
+}
+impl<IL: InstructionList, T> InstructionListCPIDynamic<IL> for T where
+    T: for<'a> InstructionListCPIDynamicAccess<'a, IL>
+{
+}
 
 /// Extension to [`InstructionListCPI`].
 /// Less efficient than [`InstructionListCPIStatic`] but can have dynamically sized account length.
 /// Use [`InstructionListCPIDynamic`].
-pub trait InstructionListCPIDynamicAccess<'a>: InstructionListCPI
+pub trait InstructionListCPIDynamicAccess<'a, IL: InstructionList>: InstructionListCPI<IL>
 where
     Self::AccountInfo: 'a,
 {
