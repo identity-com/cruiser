7b05fde7136e80b265fec4a9964972dd71915ed0
Brett Etter <buzzec@buzzec.net>
2022-04-27 11:17:27 -0500

    Added OnChainSizeWithArg

--- src/traits/on_chain_size.rs
+++ src/traits/on_chain_size.rs
@@ -11,6 +11,21 @@ pub trait OnChainSize {
     const ON_CHAIN_SIZE: usize;
 }
 
+/// Theis value can be sized with a given argument
+pub trait OnChainSizeWithArg<Arg> {
+    /// Gets the size with an argument
+    fn on_chain_size_with_arg(arg: Arg) -> usize;
+}
+
+impl<T> const OnChainSizeWithArg<()> for T
+where
+    T: OnChainSize,
+{
+    fn on_chain_size_with_arg(_arg: ()) -> usize {
+        Self::ON_CHAIN_SIZE
+    }
+}
+
 impl<T> const OnChainSize for Option<T>
 where
     T: ~const OnChainSize,
@@ -29,12 +44,40 @@ where
     const ON_CHAIN_SIZE: usize = T::ON_CHAIN_SIZE * N;
 }
 
+/// String byte length as arg
+impl const OnChainSizeWithArg<usize> for String {
+    fn on_chain_size_with_arg(arg: usize) -> usize {
+        u32::ON_CHAIN_SIZE + arg
+    }
+}
+
+impl<T> const OnChainSizeWithArg<usize> for Vec<T>
+where
+    T: OnChainSize,
+{
+    fn on_chain_size_with_arg(arg: usize) -> usize {
+        u32::ON_CHAIN_SIZE + arg * T::ON_CHAIN_SIZE
+    }
+}
+
 impl<T1, T2> const OnChainSize for (T1, T2)
 where
     T1: ~const OnChainSize,
     T2: ~const OnChainSize,
 {
     const ON_CHAIN_SIZE: usize = T1::ON_CHAIN_SIZE + T2::ON_CHAIN_SIZE;
+}
+
+impl<T1, T2, A1, A2> const OnChainSizeWithArg<(A1, A2)> for (T1, T2)
+where
+    T1: ~const OnChainSizeWithArg<A1>,
+    T2: ~const OnChainSizeWithArg<A2>,
+    A1: ~const Drop,
+    A2: ~const Drop,
+{
+    fn on_chain_size_with_arg((arg1, arg2): (A1, A2)) -> usize {
+        T1::on_chain_size_with_arg(arg1) + T2::on_chain_size_with_arg(arg2)
+    }
 }
 
 impl<T1, T2, T3> const OnChainSize for (T1, T2, T3)
@@ -44,6 +87,22 @@ where
     T3: ~const OnChainSize,
 {
     const ON_CHAIN_SIZE: usize = T1::ON_CHAIN_SIZE + T2::ON_CHAIN_SIZE + T3::ON_CHAIN_SIZE;
+}
+
+impl<T1, T2, T3, A1, A2, A3> const OnChainSizeWithArg<(A1, A2, A3)> for (T1, T2, T3)
+where
+    T1: ~const OnChainSizeWithArg<A1>,
+    T2: ~const OnChainSizeWithArg<A2>,
+    T3: ~const OnChainSizeWithArg<A3>,
+    A1: ~const Drop,
+    A2: ~const Drop,
+    A3: ~const Drop,
+{
+    fn on_chain_size_with_arg((arg1, arg2, arg3): (A1, A2, A3)) -> usize {
+        T1::on_chain_size_with_arg(arg1)
+            + T2::on_chain_size_with_arg(arg2)
+            + T3::on_chain_size_with_arg(arg3)
+    }
 }
 
 macro_rules! impl_on_chain_size_for_prim {
